88ee4aa147553ffb9ebf7807f2f419ebb9b2ff89
Rita Bennett-Chew and Ryan Putman <ryan@>
2018-06-13 11:18:10 -0700

    Add onRoll function that will check for a one, and update the turnTotal if no 1 was rolled

---
+++ js/scripts.js
@@ -0,0 +1,79 @@
+// Business Logic
+
+function Player (userName) {
+  this.name = userName;
+  this.points = 0;
+}
+
+
+Player.prototype.diceRoll = function() {
+  return (Math.ceil(Math.random() * 6))
+}
+
+// Player.prototype.takesRollChecksForOne = function(roll, wasHoldClicked) {
+//   var localTotal = 0
+//   if (roll >= 2) {
+//     localTotal += roll
+//   } else {
+//
+//   }
+//   return localTotal
+// }
+
+function takesRollChecksForOne(roll) {
+  if (roll === 1) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function updateTurnTotal(roll, wasOneRolled, turnTotal) {
+  if (!wasOneRolled) {
+    turnTotal += roll
+  } else {
+    // hide user1 dice
+    // show user2 dice
+    turnTotal = 0
+  }
+  return turnTotal
+}
+
+// User Interface
+
+$(document).ready(function() {
+
+var playerOneName = $("#nameFieldOne").val();
+var playerTwoName = $("#nameFieldTwo").val();
+var playerOne = new Player("Dave");
+var turnTotal = 0;
+
+
+  // $("button#roll").click(function() {
+    var roll = playerOne.diceRoll();
+    var isOne = takesRollChecksForOne(roll);
+    var turnTotal = updateTurnTotal(roll, isOne, turnTotal);
+    console.log(turnTotal);
+  // })
+})
+
+
+// Comments
+//
+// onRollClick(){
+//   var roll = player.diceRoll
+//   var turnTotal = 0
+//   takesRollChecksForOne(roll){
+//       wasOneRolled = true if roll = 1
+//       wasOneRolled = false if roll 2 - 6
+//       return wasOneRolled
+//   }
+//
+//   updateTurnTotal(roll, wasOneRolled)
+//     if !wasOneRolled, {update turnTotal), continues turn}
+//     else {switch players and reset turnTotal}
+// }
+//
+// onHoldClick () {
+//   player.points += turnTotal
+// }
