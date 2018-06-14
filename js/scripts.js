12c978aa1c9979206cd35a19875915eff3796010
Rita Bennett-Chew and Ryan Putman <rita@>
2018-06-13 17:10:25 -0700

    Fix bug

--- js/scripts.js
+++ js/scripts.js
@@ -30,12 +30,12 @@ function updateTurnTotal(roll, wasOneRolled, turnTotal, playerPoints, player, pl
 
 
 function switchPlayers(points, player, playerOneInput) {
+
   // business side
   checkWinCondition(points)
   if (checkWinCondition(points)){
     console.log("you win");
   }
-
   // user interface side
   if (playerOneInput === player.name) {
     $("#player-one-buttons").hide();
@@ -54,9 +54,15 @@ function checkWinCondition(points) {
 
 // User Interface
 
-function updateScoreDisplay(playerOnePoints, playerTwoPoints) {
+function updateScoreDisplay(playerOnePoints, playerTwoPoints, turnTotal) {
+  $("#total").text(turnTotal);
+  $("#turn-table").show();
   $("span#player-one-score").text(playerOnePoints)
   $("span#player-two-score").text(playerTwoPoints)
+}
+
+function updateRollDisplay(roll) {
+  $("#last-roll").text(roll);
 }
 
 $(document).ready(function() {
@@ -75,38 +81,42 @@ $(document).ready(function() {
     $(".scores-display").show();
     $("span.player-one-name").text(playerOne.name)
     $("span.player-two-name").text(playerTwo.name)
-    updateScoreDisplay(playerOne.points, playerTwo.points)
+    updateScoreDisplay(playerOne.points, playerTwo.points, turnTotal)
 
     $("button#player-one-roll").click(function() {
       var roll = playerOne.diceRoll();
+      console.log("Roll value for player 1: " + roll)
+      updateRollDisplay(roll);
       var isOne = takesRollChecksForOne(roll);
-      turnTotal = updateTurnTotal(roll, isOne, turnTotal, playerOne.points, playerOneInput)
-      updateScoreDisplay(playerOne.points, playerTwo.points)
+      turnTotal = updateTurnTotal(roll, isOne, turnTotal, playerOne.points, playerOne, playerOne.name)
+      updateScoreDisplay(playerOne.points, playerTwo.points, turnTotal)
       console.log("Turn total for player 1: " + turnTotal);
     })
 
     $("button#player-two-roll").click(function() {
       var roll = playerTwo.diceRoll();
+      console.log("Roll value for player 2: " + roll)
+      updateRollDisplay(roll);
       var isOne = takesRollChecksForOne(roll);
-      turnTotal = updateTurnTotal(roll, isOne, turnTotal, playerTwo.points, playerOneInput);
-      updateScoreDisplay(playerOne.points, playerTwo.points)
+      turnTotal = updateTurnTotal(roll, isOne, turnTotal, playerTwo.points, playerTwo, playerOne.name);
+      updateScoreDisplay(playerOne.points, playerTwo.points, turnTotal)
       console.log("Turn total for player 2: " + turnTotal);
     })
 
     $("button#player-one-hold").click(function(){
       playerOne.points += turnTotal
-      switchPlayers(playerOne.points, playerOne, playerOneInput)
-      updateScoreDisplay(playerOne.points, playerTwo.points)
-      console.log("Player 1.points: " + playerOne.points);
+      switchPlayers(playerOne.points, playerOne, playerOne.name)
       turnTotal = 0
+      updateScoreDisplay(playerOne.points, playerTwo.points, turnTotal)
+      console.log("Player 1.points: " + playerOne.points);
     })
 
     $("button#player-two-hold").click(function(){
       playerTwo.points += turnTotal
-      switchPlayers(playerTwo.points, playerTwo, playerOneInput)
-      updateScoreDisplay(playerOne.points, playerTwo.points)
-      console.log("Player 2.points: " + playerTwo.points);
+      switchPlayers(playerTwo.points, playerTwo, playerOne.name)
       turnTotal = 0
+      updateScoreDisplay(playerOne.points, playerTwo.points, turnTotal)
+      console.log("Player 2.points: " + playerTwo.points);
     })
   })
 })
